ae1947fd5a10798d7bf758edeeb17bd569abaad5
Bzil <bzchap@gmail.com>
2016-12-10 14:09:39 +0100

    WiP not functionnal

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import GitList from './container/GitList';
+
+class App extends Component {
+
+  render() {
+    return<GitList/>;
+  }
+}
+
+export default App;
---
+++ src/container/GitList.js
@@ -0,0 +1,18 @@
+/**
+ * Created by bzil on 10/12/2016.
+ */
+
+import React, { Component } from 'react';
+
+class GitList extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                Get git info from : Excilys
+            </div>
+        );
+    }
+}
+
+export default App;
